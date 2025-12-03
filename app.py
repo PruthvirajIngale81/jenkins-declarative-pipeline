735a24fe80285a91e9198f74d5be217f8b209ea8
Pruthviraj81 <pruthvirajingale0081@gmail.com>
2025-12-03 07:05:05 +0000

    3

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def create_app():
     # define routes
     @app.route('/', methods=['GET'])
     def root():
-        return "welcome to the flask application"
+        return "welcome to the flask application:V8"
     
     @app.route('/health', methods=['GET'])
     def health():
